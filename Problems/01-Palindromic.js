88003b5e34c088750d5e34cde170d9f4cca6d00b
Matheus da Rocha Sena <matheus.sena@dataprev.gov.br>
2025-08-27 16:17:37 -0300

    Adicionando código de Palindrome

---
+++ Problems/01-Palindromic.js
@@ -0,0 +1,32 @@
+const Palindromic = (str) => {
+    /*
+    |RACECAR| número impar
+    0123456 
+
+    RACEECAR número para de caracteres
+    01234567
+
+    número de equivalências em uma String
+
+    Para saber se não é um palindrome devemos percorrer até o meio para ter certeza
+
+    Se tivermos uma sequência de equivalências até o meio da string 
+    maior do que duas temos uma Palindrome
+
+    hellosannasmith
+
+    */
+
+    function IsPalindromic () {
+    
+    for (let i = 0; i < str.length ; i++){
+    let left;
+    let rigt;
+
+    }
+    }
+    
+    
+    return str
+}
+
