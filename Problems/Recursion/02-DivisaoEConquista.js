8bd7fe3c6f8d15b453b80447fe2f3b577fe97b52
matheus da rocha sena <matheus.sena@dataprev.gov.br>
2025-09-10 15:54:03 -0300

    Adding the Divisão e Conquista solution

---
+++ Problems/Recursion/02-DivisaoEConquista.js
@@ -0,0 +1,16 @@
+
+function maiorSubArray (vetor) {
+    let soma = 0;
+    let MaiorSubArr = [];
+    for (let i = 0; i < vetor.length; i++){
+        if (vetor[i] > 0){
+            if (MaiorSubArr.length === 0){
+                MaiorSubArr.push(vetor[i]);
+            } else if (vetor[i-1] > 0 ){
+
+            }
+        }
+    }
+
+    return maior 
+}
