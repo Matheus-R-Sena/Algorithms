fe90ecf83e4222ba722b571aa39dea64204fd891
matheus da rocha sena <matheus.sena@dataprev.gov.br>
2025-09-16 15:47:29 -0300

    Adicionandomatriz de arrays.

--- Problems/Recursion/02-DivisaoEConquista.js
+++ Problems/Recursion/02-DivisaoEConquista.js
@@ -1,5 +1,22 @@
+// 1. Crio um array de arrays. Uma matriz (todos vazios)
+// 2. Armazeno todas as possibilidades dentro desta matriz, cada array dentro dela será uma possibilidade 
+// 3. Eu chamo uma função que retorna a soma do subarray
+// 4. Essas possibilidades são criadas por um append dos elementos do array base nesses arrays da Matriz.
+
+
 
 function maiorSubArray (vetor) {
+    
+    const MatrizBase = [];
+    
+    for (let i = 0; i < vetor.length; i++){
+        const arrayTemp = [];
+        arrayTemp.push(vetor[i]);
+        MatrizBase.push(arrayTemp);
+    }
+    
+    
+    
     let soma = 0;
     let MaiorSubArr = [];
     for (let i = 0; i < vetor.length; i++){
