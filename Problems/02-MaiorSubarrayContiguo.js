e4fbf933b78aa702e109e5f285330871243fd40d
matheus da rocha sena <matheus.sena@dataprev.gov.br>
2025-10-23 11:57:03 -0300

    Adicionando a solução quadrática funcional

--- Problems/02-MaiorSubarrayContiguo.js
+++ Problems/02-MaiorSubarrayContiguo.js
@@ -47,10 +47,11 @@ const biggerSubarrayVersion01 = (arr) => {
 
 function biggerSubarrayVersion02 (arr) {
 
-    let somaMaxima = 0;
-    for(let i = 0; i < arr; i++){
+    let somaMaxima = arr[0];
+
+    for(let i = 0; i < arr.length; i++){   
         let somaParcial = 0;
-        for (let j = i; j < arr; j++){
+        for (let j = i; j < arr.length; j++){
         somaParcial += arr[j]
         if (somaParcial > somaMaxima){
             somaMaxima = somaParcial;
@@ -60,13 +61,36 @@ function biggerSubarrayVersion02 (arr) {
     return somaMaxima;
 }
 
+//testes para a solção quadrática
+
+
+//const vetor = [5]                           //1)  Resultado: 5 PASSOU!!
+//const vetor = [1, 2, 3, 4, 5]               //2)  Resultado: 15 PASSOU!!   
+//const vetor = [-1, -2, -3, -4]              //3)  Resultado: -1 PASSOU!!
+//const vetor = [4, -1, 2, 1]                 //4)  Resultado: 6 PASSOU!! 
+//const vetor = [2, -8, 3, -2, 4, -10]        //5)  Resultado: 5 PASSOU!!
+//const vetor = [1, -3, 2, 1, -1] //8)        //6)  Resultado: 3 PASSOU!!
+//const vetor = [1, 2, -2, 5, -1, 4, 12]      //7)  Resultado: 21 PASSOU!!
+//const vetor = [0, 0, 0, 0]                  //8)  Resultado: 0 PASSOU!!
+//const vetor = [3, -1, -1, -1, 5]            //9)  Resultado: 5 PASSOU!!
+//const vetor = [8, -19, 5, -4, 20]           //10) Resultado: 21 ASSOU!!
+//const vetor = [2, 3, -6, 4, 2, -1, 2, -1]   //11) Resultado: 7 PASSOU!!
+//const vetor = [-2, -3, 4, -1, -2, 1, 5, -3] //12) Resultado: 7 PASSOU!!
+//const vetor = [100, -90, 50, -60, 70]       //13) Resultado: 100 PASSOU!!
+//const vetor = [1, -1, 1, -1, 1, -1, 10]     //14) Resultado: 10 PASSOU!!
+//const vetor = [2, 3, 4, -2, -3, 5, -3, 2, 2]//15) Resultado: 10 PASSOU!!
+
+//Solução linear
+function biggerSubarrayVersion03 (arr) {
 
 
 
-const vetor = [1,-2,3,-50,5,6]
+    return somaMaxima;
+}
 
 
-//21 subarrays
+const vetor = [1,-2,3,-50,5,6] //Resultado: 11
+
 console.log(biggerSubarrayVersion02(vetor));
 
 
