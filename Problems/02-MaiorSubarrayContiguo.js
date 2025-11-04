14e12c1d69486b19956bd23ada44e9cfd8cb2a7a
matheus da rocha sena <matheus.sena@dataprev.gov.br>
2025-11-04 11:19:41 -0300

    Adicionando solução mais correta, porem ainda parcial de O(N).

--- Problems/02-MaiorSubarrayContiguo.js
+++ Problems/02-MaiorSubarrayContiguo.js
@@ -80,26 +80,36 @@ function biggerSubarrayVersion02 (arr) {
 //const vetor = [1, -1, 1, -1, 1, -1, 10]     //14) Resultado: 10 PASSOU!!
 //const vetor = [2, 3, 4, -2, -3, 5, -3, 2, 2]//15) Resultado: 10 PASSOU!!
 
-//Solução linear
+//Solução linear Aloritmo de Kadane
 function biggerSubarrayVersion03 (arr) {
-
+//somaMaxima = -1
     let somaMaxima = arr[0];
-
     for (let i = 0; i < arr.length; i++){
-        //Aqui está o problema preciso que somaParcial zere a cada loop
-        let somaParcial = 0;
-        somaParcial += arr[i];
-        if (somaParcial > somaMaxima){
-            somaMaxima = somaParcial;
-        }
-
+        if (i === 0) continue;
+        somaMaxima += arr[i];
+        if (somaMaxima < arr[i]) somaMaxima = arr[i];
     }
     return somaMaxima;
 }
 
+//const vetor = [5]                           //1)  Resultado: 5 PASSOU!!
+//const vetor = [1, 2, 3, 4, 5]               //2)  Resultado: 15 PASSOU!!   
+const vetor = [-1, -2, -3, -4]              //3)  Resultado: -1 PASSOU!!
+//const vetor = [4, -1, 2, 1]                 //4)  Resultado: 6 PASSOU!! 
+//const vetor = [2, -8, 3, -2, 4, -10]        //5)  Resultado: 5 PASSOU!!
+//const vetor = [1, -3, 2, 1, -1] //8)        //6)  Resultado: 3 PASSOU!!
+//const vetor = [1, 2, -2, 5, -1, 4, 12]      //7)  Resultado: 21 PASSOU!!
+//const vetor = [0, 0, 0, 0]                  //8)  Resultado: 0 PASSOU!!
+//const vetor = [3, -1, -1, -1, 5]            //9)  Resultado: 5 PASSOU!!
+//const vetor = [8, -19, 5, -4, 20]           //10) Resultado: 21 ASSOU!!
+//const vetor = [2, 3, -6, 4, 2, -1, 2, -1]   //11) Resultado: 7 PASSOU!!
+//const vetor = [-2, -3, 4, -1, -2, 1, 5, -3] //12) Resultado: 7 PASSOU!!
+//const vetor = [100, -90, 50, -60, 70]       //13) Resultado: 100 PASSOU!!
+//const vetor = [1, -1, 1, -1, 1, -1, 10]     //14) Resultado: 10 PASSOU!!
+//const vetor = [2, 3, 4, -2, -3, 5, -3, 2, 2]//15) Resultado: 10 PASSOU!!
 
-const vetor = [1,-2,3,-50,5,6] //Resultado: 11
+//const vetor = [1,-2,3,-50,5,6] //Resultado: 11
 
-console.log(biggerSubarrayVersion02(vetor));
+console.log(biggerSubarrayVersion03(vetor));
 
 
